55d90e3b7456b573c4d1dc7fda03c0e7357f3e72
PriyanshBordia <priyanshbordia1@gmail.com>
2021-04-14 19:54:10 +0530

    color pallete changed

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM Loaded..!!');
 
     const btnHamburger = document.querySelector('#btnHamburger');
+    const body = document.querySelector('body');
     const header = document.querySelector('.header');
     const fadeElements = document.querySelectorAll('.has-fade');
 
@@ -12,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (header.classList.contains('open'))
         {
             header.classList.remove('open');
+            body.classList.remove('no-scroll');
             fadeElements.forEach((element) => {
                 element.classList.remove('fade-in');
                 element.classList.add('fade-out');
@@ -22,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
         else
         {
             header.classList.add('open');
+            body.classList.add('no-scroll');
             fadeElements.forEach((element) => {
                 element.classList.remove('fade-out');
                 element.classList.add('fade-in');
