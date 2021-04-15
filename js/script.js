ce06fa523b3967f4cdd7b15e10272c3718befe9e
PriyanshBordia <priyanshbordia1@gmail.com>
2021-04-15 13:39:43 +0530

    index styling changed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
     console.log('DOM Loaded..!!');
 
+    var opacity = 0;
+    var intervalId = 0;
+
+    window.onload = fadeout;
+
+    function fadeout()
+    {
+        setInterval(hide, 200);
+    }
+
+    function hide()
+    {
+        var wrapper = document.getElementById("body");
+
+        opacity = Number(window.getComputedStyle(wrapper).getPropertyValue("opacity"));
+
+        const anim = anime.timeline({
+            loop: true,
+            direction: 'alternate',
+        });
+
+        anim
+            .add({
+                targets: '#hexagon path',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeInOutQuart',
+                duration: 2000,
+                delay: function (el, i) { return i * 250 },
+            })
+            .add({
+                targets: '#hexagon #B',
+                duration: 1000,
+                opacity: 1,
+                easing: 'easeInOutQuart'
+            });
+            
+        window.getComputedStyle(wrapper).setPropertyValue("opacity") = 1
+    }
+
     const btnHamburger = document.querySelector('#btnHamburger');
     const body = document.querySelector('body');
     const header = document.querySelector('.header');
@@ -36,30 +75,4 @@ document.addEventListener('DOMContentLoaded', () => {
         var audio = new Audio('media/music/chime.mp3');
         audio.play();
     };
-
-    // var opacity = 0;
-    // var intervalId = 0;
-
-    // window.onload = fadeout;
-
-    // function fadeout()
-    // {
-    //     setInterval(hide, 200);
-    // }
-
-    // function hide()
-    // {
-    //     var wrapper = document.getElementById("body");
-
-    //     opacity = Number(window.getComputedStyle(wrapper).getPropertyValue("opacity"));
-
-    //     if (opacity > 0) 
-    //     {
-    //         opacity = opacity - 0.1;
-    //     }
-
-    //     else {
-    //         clearInterval(intervalId);
-    //     }
-    // }
 });
