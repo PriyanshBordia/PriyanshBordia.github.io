f31c50f86295ecf9645d695b30e2dc10eee37b0e
PriyanshBordia <priyanshbordia1@gmail.com>
2021-04-21 12:10:47 +0530

    hex animation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     console.log('DOM Loaded..!!');
 
+    const anim = anime.timeline({
+        loop: true,
+        direction: 'alternate',
+    });
+
+    anim
+        .add({
+            targets: '#hexagon path',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'easeInOutQuart',
+            duration: 2000,
+            delay: function (el, i) { return i * 250 },
+        })
+        .add({
+            targets: '#hexagon #B',
+            duration: 1000,
+            opacity: 1,
+            easing: 'easeInOutQuart'
+        });
+        
     function play() {
         var audio = new Audio('../media/music/chime.mp3');
         audio.play();
