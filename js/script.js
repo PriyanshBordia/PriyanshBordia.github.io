aa862b4368f37626d0b9366577e060621ad7690f
PriyanshBordia <priyanshbordia1@gmail.com>
2021-04-29 22:43:57 +0530

    onload animation

--- js/script.js
+++ js/script.js
@@ -5,28 +5,7 @@ function play() {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    console.log('DOM Loaded..!!');
-
-    const anim = anime.timeline({
-        loop: true,
-        direction: 'alternate',
-    });
-
-    anim
-        .add({
-            targets: '#hexagon path',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            easing: 'easeInOutQuart',
-            duration: 2000,
-            delay: function (el, i) { return i * 250 },
-        })
-        .add({
-            targets: '#hexagon #B',
-            duration: 1000,
-            opacity: 1,
-            easing: 'easeInOutQuart'
-        });
-        
+    console.log('DOM Loaded..!!');    
 
     const btnHamburger = document.querySelector('#btnHamburger');
     const body = document.querySelector('body');
