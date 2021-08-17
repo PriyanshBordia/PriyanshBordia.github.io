6e7eaa8340d71b1bc66afa48eb9ec135a456b423
Restyled.io <commits@restyled.io>
2021-08-17 07:40:29 +0000

    Restyled by prettier

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 function play() {
-  var audio = new Audio('./media/music/chime.mp3');
+  var audio = new Audio("./media/music/chime.mp3");
   audio.play();
-};
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM Loaded..!!');
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM Loaded..!!");
 
   const typingEffect = document.querySelector(".main__typingText");
 
-  const typingText =
-      [ "Software Developer", "Student", "Tech Geek", "Footballer" ];
+  const typingText = [
+    "Software Developer",
+    "Student",
+    "Tech Geek",
+    "Footballer",
+  ];
   const typingDelay = 200;
   const eraseDelay = 100;
   const newTextDelay = 2000;
@@ -24,9 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
       charIdx++;
 
       setTimeout(type, typingDelay);
-    }
-
-    else {
+    } else {
       setTimeout(erase, newTextDelay);
     }
   }
@@ -36,67 +38,64 @@ document.addEventListener('DOMContentLoaded', () => {
       typingEffect.textContent = typingText[idx].substring(0, charIdx - 1);
       charIdx--;
       setTimeout(erase, eraseDelay);
-    }
-
-    else {
+    } else {
       idx++;
-      if (idx >= typingText.length)
-        idx = 0;
+      if (idx >= typingText.length) idx = 0;
 
       setTimeout(type, typingDelay);
     }
   }
 
-  const btnHamburger = document.querySelector('#btnHamburger');
-  const body = document.querySelector('body');
-  const header = document.querySelector('.header');
-  const footer = document.querySelector('.footer');
-  const fadeElements = document.querySelectorAll('.has-fade');
+  const btnHamburger = document.querySelector("#btnHamburger");
+  const body = document.querySelector("body");
+  const header = document.querySelector(".header");
+  const footer = document.querySelector(".footer");
+  const fadeElements = document.querySelectorAll(".has-fade");
 
   btnHamburger.onclick = () => {
-    if (header.classList.contains('open')) {
-      header.classList.remove('open');
-      body.classList.remove('no-scroll');
+    if (header.classList.contains("open")) {
+      header.classList.remove("open");
+      body.classList.remove("no-scroll");
       fadeElements.forEach((element) => {
-        element.classList.remove('fade-in');
-        element.classList.add('fade-out');
+        element.classList.remove("fade-in");
+        element.classList.add("fade-out");
       });
 
-      footer.classList.remove('fixed-bottom');
-    }
-
-    else {
-      header.classList.add('open');
-      body.classList.add('no-scroll');
+      footer.classList.remove("fixed-bottom");
+    } else {
+      header.classList.add("open");
+      body.classList.add("no-scroll");
       fadeElements.forEach((element) => {
-        element.classList.remove('fade-out');
-        element.classList.add('fade-in');
+        element.classList.remove("fade-out");
+        element.classList.add("fade-in");
       });
 
-      footer.classList.add('fixed-bottom');
+      footer.classList.add("fixed-bottom");
     }
   };
 
-  const tabs = document.querySelector('.experience__tabs');
+  const tabs = document.querySelector(".experience__tabs");
 
-  tabs.addEventListener('click', e => handleClick(e));
+  tabs.addEventListener("click", (e) => handleClick(e));
 
   function handleClick(e) {
     const target = e.target;
     const tabNum = target.dataset.tab;
-    const activeTab = document.querySelector('.experience__tabs .active');
-    const activeContent =
-        document.querySelector('.experience__content .visible');
+    const activeTab = document.querySelector(".experience__tabs .active");
+    const activeContent = document.querySelector(
+      ".experience__content .visible"
+    );
     const currentContent = document.querySelector(
-        `.experience__content__section[data-tab='${tabNum}']`);
+      `.experience__content__section[data-tab='${tabNum}']`
+    );
 
     if (!tabNum) {
       return;
     }
 
-    activeTab.classList.remove('active');
-    target.classList.add('active');
-    activeContent.classList.remove('visible');
-    currentContent.classList.add('visible');
-  };
+    activeTab.classList.remove("active");
+    target.classList.add("active");
+    activeContent.classList.remove("visible");
+    currentContent.classList.add("visible");
+  }
 });
--- tools/js/todo.js
+++ tools/js/todo.js
@@ -1,167 +1,167 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   setDisabled(true);
 
   var todoList = new Array();
   var notodoList = new Array();
 
-  if (!localStorage.getItem('todoList'))
-    localStorage.setItem('todoList', JSON.stringify(todoList));
+  if (!localStorage.getItem("todoList"))
+    localStorage.setItem("todoList", JSON.stringify(todoList));
   else {
-    todoList = JSON.parse(localStorage.getItem('todoList'));
+    todoList = JSON.parse(localStorage.getItem("todoList"));
 
     for (var i = 0; i < todoList.length; i++) {
       const li = addTaskToList(todoList[i]);
-      document.querySelector('.todo__list').append(li);
+      document.querySelector(".todo__list").append(li);
 
-      const last_li = document.querySelector('.todo__list > li:last-child');
+      const last_li = document.querySelector(".todo__list > li:last-child");
 
-      const bin = document.createElement('i');
-      bin.className = 'far fa-trash-alt';
+      const bin = document.createElement("i");
+      bin.className = "far fa-trash-alt";
       // bin.value = 'b' + String(i);
-      bin.addEventListener('click', () => removeTaskFromList(li, todoList, i));
+      bin.addEventListener("click", () => removeTaskFromList(li, todoList, i));
       last_li.append(bin);
 
-      const pencil = document.createElement('i');
-      pencil.className = 'fas fa-pencil-alt';
+      const pencil = document.createElement("i");
+      pencil.className = "fas fa-pencil-alt";
       // pencil.value = 'p' + String(i);
       // pencil.addEventListener('click', () => editTask(li, todoList, i));
       last_li.append(pencil);
     }
   }
 
-  if (!localStorage.getItem('notodoList'))
-    localStorage.setItem('notodoList', JSON.stringify(notodoList));
+  if (!localStorage.getItem("notodoList"))
+    localStorage.setItem("notodoList", JSON.stringify(notodoList));
   else {
-    notodoList = JSON.parse(localStorage.getItem('notodoList'));
+    notodoList = JSON.parse(localStorage.getItem("notodoList"));
 
     for (var i = 0; i < notodoList.length; i++) {
       const li = addTaskToList(notodoList[i]);
-      document.querySelector('.notodo__list').append(li);
+      document.querySelector(".notodo__list").append(li);
 
-      const last_li = document.querySelector('.notodo__list > li:last-child');
+      const last_li = document.querySelector(".notodo__list > li:last-child");
 
-      const bin = document.createElement('i');
-      bin.className = 'far fa-trash-alt';
+      const bin = document.createElement("i");
+      bin.className = "far fa-trash-alt";
       // bin.value = 'b' + String(i);
-      bin.addEventListener('click',
-                           () => removeTaskFromList(li, notodoList, i));
+      bin.addEventListener("click", () =>
+        removeTaskFromList(li, notodoList, i)
+      );
       last_li.append(bin);
 
-      const pencil = document.createElement('i');
-      pencil.className = 'fas fa-pencil-alt';
+      const pencil = document.createElement("i");
+      pencil.className = "fas fa-pencil-alt";
       // pencil.value = 'p' + String(i);
       // pencil.addEventListener('click', () => editTask(li, notodoList, i));
       last_li.append(pencil);
     }
   }
 
-  document.querySelector('#task').onkeyup = () => {
-    if ((document.querySelector('#task').value.trim()).length > 0)
+  document.querySelector("#task").onkeyup = () => {
+    if (document.querySelector("#task").value.trim().length > 0)
       setDisabled(false);
-    else
-      setDisabled(true);
+    else setDisabled(true);
   };
 
   // const button = document.querySelectorAll('button');
 
-  document.querySelector('#todo').onclick = () => {
-    task = document.querySelector('#task').value.trim();
+  document.querySelector("#todo").onclick = () => {
+    task = document.querySelector("#task").value.trim();
 
     const li = addTaskToList(task);
-    document.querySelector('.todo__list').append(li);
+    document.querySelector(".todo__list").append(li);
 
-    const last_li = document.querySelector('.todo__list > li:last-child');
+    const last_li = document.querySelector(".todo__list > li:last-child");
 
-    const bin = document.createElement('i');
-    bin.className = 'far fa-trash-alt';
+    const bin = document.createElement("i");
+    bin.className = "far fa-trash-alt";
     // bin.value = 'b' + String(todoList.length - 1);
-    bin.addEventListener(
-        'click', () => removeTaskFromList(li, todoList, todoList.length));
+    bin.addEventListener("click", () =>
+      removeTaskFromList(li, todoList, todoList.length)
+    );
     last_li.append(bin);
 
-    const pencil = document.createElement('i');
-    pencil.className = 'fas fa-pencil-alt';
+    const pencil = document.createElement("i");
+    pencil.className = "fas fa-pencil-alt";
     // pencil.value = 'p' + String(todoList.length - 1);
     // pencil.addEventListener('click', () => editTask(li, todoList,
     // todoList.length));
     last_li.append(pencil);
 
     todoList.push(String(task));
-    localStorage.setItem('todoList', JSON.stringify(todoList));
+    localStorage.setItem("todoList", JSON.stringify(todoList));
 
     setDisabled(true);
 
     console.log(task);
     console.log(todoList[todoList.length - 1]);
 
-    document.querySelector('#task').value = '';
+    document.querySelector("#task").value = "";
 
     return false;
   };
 
-  document.querySelector('#notodo').onclick = () => {
-    task = document.querySelector('#task').value.trim();
+  document.querySelector("#notodo").onclick = () => {
+    task = document.querySelector("#task").value.trim();
 
     const li = addTaskToList(task);
-    document.querySelector('.notodo__list').append(li);
+    document.querySelector(".notodo__list").append(li);
 
-    const last_li = document.querySelector('.notodo__list > li:last-child');
+    const last_li = document.querySelector(".notodo__list > li:last-child");
 
-    const bin = document.createElement('i');
-    bin.className = 'far fa-trash-alt';
+    const bin = document.createElement("i");
+    bin.className = "far fa-trash-alt";
     // bin.value = 'b' + String(notodoList.length - 1);
-    bin.addEventListener('click', () => removeTaskFromList(
-                                      li, notodoList, notodoList.length - 1));
+    bin.addEventListener("click", () =>
+      removeTaskFromList(li, notodoList, notodoList.length - 1)
+    );
     last_li.append(bin);
 
-    const pencil = document.createElement('i');
-    pencil.className = 'fas fa-pencil-alt';
+    const pencil = document.createElement("i");
+    pencil.className = "fas fa-pencil-alt";
     // pencil.value = 'p' + String(notodoList.length - 1);
     // pencil.addEventListener('click', () => editTask(li, notodoList,
     // notodoList.length));
     last_li.append(pencil);
 
     notodoList.push(String(task));
-    localStorage.setItem('notodoList', JSON.stringify(notodoList));
+    localStorage.setItem("notodoList", JSON.stringify(notodoList));
 
     setDisabled(true);
 
-    document.querySelector('#task').value = '';
+    document.querySelector("#task").value = "";
 
     return false;
   };
 
   function setDisabled(value) {
-    document.querySelector('#todo').disabled = value;
-    document.querySelector('#notodo').disabled = value;
-  };
+    document.querySelector("#todo").disabled = value;
+    document.querySelector("#notodo").disabled = value;
+  }
 
-  document.querySelectorAll('.bin').onclick = () => {
-    console.log('bin')
+  document.querySelectorAll(".bin").onclick = () => {
+    console.log("bin");
     // removeTaskFromList( , 'todoList');
   };
 
-  document.querySelectorAll('.pencil').onclick =
-      () => { console.log('pencil'); };
-
-  function addTaskToList(taskName) {
-
-    var li = document.createElement('li');
-    li.innerHTML = taskName;
-    return li;
+  document.querySelectorAll(".pencil").onclick = () => {
+    console.log("pencil");
   };
 
-  function editTask(input, list, taskName) {
-    if (input.disabled == true)
-      input.disabled = false;
+  function addTaskToList(taskName) {
+    var li = document.createElement("li");
+    li.innerHTML = taskName;
+    return li;
+  }
 
+  function editTask(input, list, taskName) {
+    if (input.disabled == true) input.disabled = false;
     else {
       input.disabled = true;
       let indexOf = list.indexOf(taskName);
       list[indexOf] = input.value;
       localStorage.setItem(list, JSON.stringify(list));
     }
-  };
+  }
 
   function removeTaskFromList(li, list, taskId) {
     try {
@@ -172,15 +172,10 @@ document.addEventListener('DOMContentLoaded', () => {
       if (taskId >= 0) {
         todoList.splice(taskId, 1);
         console.log(todoList);
-        localStorage.setItem('todoList', JSON.stringify(todoList));
-      }
-
-      else
-        console.log('Index out of bound.!!');
-    }
-
-    catch (error) {
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+      } else console.log("Index out of bound.!!");
+    } catch (error) {
       console.error();
     }
-  };
+  }
 });
