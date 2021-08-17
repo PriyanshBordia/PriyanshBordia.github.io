1f11e3bffb1d44e1be2eed6087b432e9beb58569
Restyled.io <commits@restyled.io>
2021-08-17 07:40:20 +0000

    Restyled by clang-format

--- js/script.js
+++ js/script.js
@@ -1,110 +1,102 @@
 function play() {
-    var audio = new Audio('./media/music/chime.mp3');
-    audio.play();
+  var audio = new Audio('./media/music/chime.mp3');
+  audio.play();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM Loaded..!!');
 
-    console.log('DOM Loaded..!!');    
+  const typingEffect = document.querySelector(".main__typingText");
 
-	const typingEffect = document.querySelector(".main__typingText");
+  const typingText =
+      [ "Software Developer", "Student", "Tech Geek", "Footballer" ];
+  const typingDelay = 200;
+  const eraseDelay = 100;
+  const newTextDelay = 2000;
+  let idx = 0;
+  let charIdx = 0;
 
-	const typingText = ["Software Developer", "Student", "Tech Geek", "Footballer"];
-	const typingDelay = 200;
-	const eraseDelay = 100;
-	const newTextDelay = 2000;
-	let idx = 0;
-	let charIdx = 0;
+  type();
 
-	type();
+  function type() {
+    if (charIdx < typingText[idx].length) {
+      typingEffect.textContent += typingText[idx].charAt(charIdx);
+      charIdx++;
 
-	function type()
-	{
-		if (charIdx < typingText[idx].length)
-		{
-			typingEffect.textContent += typingText[idx].charAt(charIdx);
-			charIdx++;
+      setTimeout(type, typingDelay);
+    }
 
-			setTimeout(type, typingDelay);
-		}
+    else {
+      setTimeout(erase, newTextDelay);
+    }
+  }
 
-		else
-		{
-			setTimeout(erase, newTextDelay);
-		}
-	}
+  function erase() {
+    if (charIdx > 0) {
+      typingEffect.textContent = typingText[idx].substring(0, charIdx - 1);
+      charIdx--;
+      setTimeout(erase, eraseDelay);
+    }
 
-	function erase()
-	{
-		if (charIdx > 0)
-		{
-			typingEffect.textContent = typingText[idx].substring(0, charIdx - 1);
-			charIdx--;
-			setTimeout(erase, eraseDelay);
-		}
+    else {
+      idx++;
+      if (idx >= typingText.length)
+        idx = 0;
 
-		else
-		{
-			idx++;
-			if (idx >= typingText.length)
-				idx = 0;
+      setTimeout(type, typingDelay);
+    }
+  }
 
-			setTimeout(type, typingDelay);
-		}
-	}
+  const btnHamburger = document.querySelector('#btnHamburger');
+  const body = document.querySelector('body');
+  const header = document.querySelector('.header');
+  const footer = document.querySelector('.footer');
+  const fadeElements = document.querySelectorAll('.has-fade');
 
-    const btnHamburger = document.querySelector('#btnHamburger');
-    const body = document.querySelector('body');
-    const header = document.querySelector('.header');
-    const footer = document.querySelector('.footer');
-    const fadeElements = document.querySelectorAll('.has-fade');
+  btnHamburger.onclick = () => {
+    if (header.classList.contains('open')) {
+      header.classList.remove('open');
+      body.classList.remove('no-scroll');
+      fadeElements.forEach((element) => {
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+      });
 
+      footer.classList.remove('fixed-bottom');
+    }
 
-    btnHamburger.onclick = () => {
+    else {
+      header.classList.add('open');
+      body.classList.add('no-scroll');
+      fadeElements.forEach((element) => {
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+      });
 
-        if (header.classList.contains('open'))
-        {
-            header.classList.remove('open');
-            body.classList.remove('no-scroll');
-            fadeElements.forEach((element) => {
-                element.classList.remove('fade-in');
-                element.classList.add('fade-out');
-            });
+      footer.classList.add('fixed-bottom');
+    }
+  };
 
-            footer.classList.remove('fixed-bottom');
-        }
+  const tabs = document.querySelector('.experience__tabs');
 
-        else
-        {
-            header.classList.add('open');
-            body.classList.add('no-scroll');
-            fadeElements.forEach((element) => {
-                element.classList.remove('fade-out');
-                element.classList.add('fade-in');
-            });
+  tabs.addEventListener('click', e => handleClick(e));
 
-            footer.classList.add('fixed-bottom');
-        }
-    };
+  function handleClick(e) {
+    const target = e.target;
+    const tabNum = target.dataset.tab;
+    const activeTab = document.querySelector('.experience__tabs .active');
+    const activeContent =
+        document.querySelector('.experience__content .visible');
+    const currentContent = document.querySelector(
+        `.experience__content__section[data-tab='${tabNum}']`);
 
-	const tabs = document.querySelector('.experience__tabs');
+    if (!tabNum) {
+      return;
+    }
 
-	tabs.addEventListener('click', e => handleClick(e));
-
-	function handleClick(e) {
-		const target = e.target;
-		const tabNum = target.dataset.tab;
-		const activeTab = document.querySelector('.experience__tabs .active');
-		const activeContent = document.querySelector('.experience__content .visible');
-		const currentContent = document.querySelector(`.experience__content__section[data-tab='${tabNum}']`);
-
-		if (!tabNum) {
-			return;
-		}
-
-		activeTab.classList.remove('active');
-		target.classList.add('active');
-		activeContent.classList.remove('visible');
-		currentContent.classList.add('visible');
-	};
+    activeTab.classList.remove('active');
+    target.classList.add('active');
+    activeContent.classList.remove('visible');
+    currentContent.classList.add('visible');
+  };
 });
