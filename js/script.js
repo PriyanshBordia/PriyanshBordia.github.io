abc5ad646a1d9fd28cec2eadb62b2a3c88f80a50
PriyanshBordia <priyanshbordia1@gmail.com>
2021-11-09 00:07:32 +0530

    t-001

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const typingEffect = document.querySelector(".main__typingText");
 
   const typingText = [
-    "Engineer Software",
+	"Engineer Software",
 	"Develop Software",
-    "am a Student",
-    "am a Tech Geek",
-    "play Football",
+	"am a Student",
+	"am a Tech Geek",
+	"play Football",
   ];
   const typingDelay = 200;
   const eraseDelay = 150;
@@ -24,27 +24,27 @@ document.addEventListener("DOMContentLoaded", () => {
   type();
 
   function type() {
-    if (charIdx < typingText[idx].length) {
-      typingEffect.textContent += typingText[idx].charAt(charIdx);
-      charIdx++;
+	if (charIdx < typingText[idx].length) {
+	  typingEffect.textContent += typingText[idx].charAt(charIdx);
+	  charIdx++;
 
-      setTimeout(type, typingDelay);
-    } else {
-      setTimeout(erase, newTextDelay);
-    }
+	  setTimeout(type, typingDelay);
+	} else {
+	  setTimeout(erase, newTextDelay);
+	}
   }
 
   function erase() {
-    if (charIdx > 0) {
-      typingEffect.textContent = typingText[idx].substring(0, charIdx - 1);
-      charIdx--;
-      setTimeout(erase, eraseDelay);
-    } else {
-      idx++;
-      if (idx >= typingText.length) idx = 0;
+	if (charIdx > 0) {
+	  typingEffect.textContent = typingText[idx].substring(0, charIdx - 1);
+	  charIdx--;
+	  setTimeout(erase, eraseDelay);
+	} else {
+	  idx++;
+	  if (idx >= typingText.length) idx = 0;
 
-      setTimeout(type, typingDelay);
-    }
+	  setTimeout(type, typingDelay);
+	}
   }
 
   const btnHamburger = document.querySelector("#btnHamburger");
@@ -54,25 +54,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const fadeElements = document.querySelectorAll(".has-fade");
 
   btnHamburger.onclick = () => {
-    if (header.classList.contains("open")) {
-      header.classList.remove("open");
-      body.classList.remove("no-scroll");
-      fadeElements.forEach((element) => {
-        element.classList.remove("fade-in");
-        element.classList.add("fade-out");
-      });
+	if (header.classList.contains("open")) {
+	  header.classList.remove("open");
+	  body.classList.remove("no-scroll");
+	  fadeElements.forEach((element) => {
+		element.classList.remove("fade-in");
+		element.classList.add("fade-out");
+	  });
 
-      footer.classList.remove("fixed-bottom");
-    } else {
-      header.classList.add("open");
-      body.classList.add("no-scroll");
-      fadeElements.forEach((element) => {
-        element.classList.remove("fade-out");
-        element.classList.add("fade-in");
-      });
+	  footer.classList.remove("fixed-bottom");
+	} else {
+	  header.classList.add("open");
+	  body.classList.add("no-scroll");
+	  fadeElements.forEach((element) => {
+		element.classList.remove("fade-out");
+		element.classList.add("fade-in");
+	  });
 
-      footer.classList.add("fixed-bottom");
-    }
+	  footer.classList.add("fixed-bottom");
+	}
   };
 
   const tabs = document.querySelector(".experience__tabs");
@@ -80,23 +80,23 @@ document.addEventListener("DOMContentLoaded", () => {
   tabs.addEventListener("click", (e) => handleClick(e));
 
   function handleClick(e) {
-    const target = e.target;
-    const tabNum = target.dataset.tab;
-    const activeTab = document.querySelector(".experience__tabs .active");
-    const activeContent = document.querySelector(
-      ".experience__content .visible"
-    );
-    const currentContent = document.querySelector(
-      `.experience__content__section[data-tab='${tabNum}']`
-    );
+	const target = e.target;
+	const tabNum = target.dataset.tab;
+	const activeTab = document.querySelector(".experience__tabs .active");
+	const activeContent = document.querySelector(
+	  ".experience__content .visible"
+	);
+	const currentContent = document.querySelector(
+	  `.experience__content__section[data-tab='${tabNum}']`
+	);
 
-    if (!tabNum) {
-      return;
-    }
+	if (!tabNum) {
+	  return;
+	}
 
-    activeTab.classList.remove("active");
-    target.classList.add("active");
-    activeContent.classList.remove("visible");
-    currentContent.classList.add("visible");
+	activeTab.classList.remove("active");
+	target.classList.add("active");
+	activeContent.classList.remove("visible");
+	currentContent.classList.add("visible");
   }
 });
