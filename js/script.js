2491789b3d4b5eafd1d58c58be58008273573d8e
PriyanshBordia <62763160+PriyanshBordia@users.noreply.github.com>
2022-02-15 10:55:11 +0530

    added dynamic dates

--- js/script.js
+++ js/script.js
@@ -1,36 +1,37 @@
 function play() {
-  var audio = new Audio("./media/music/chime.mp3");
-  audio.play();
+	var audio = new Audio("./media/music/chime.mp3");
+	audio.play();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM Loaded..!!");
 
-  const btnHamburger = document.querySelector("#btnHamburger");
-  const body = document.querySelector("body");
-  const header = document.querySelector(".header");
-  const footer = document.querySelector(".footer");
-  const fadeElements = document.querySelectorAll(".has-fade");
+  	console.log("DOM Loaded..!!");
 
-  btnHamburger.onclick = () => {
+	const btnHamburger = document.querySelector("#btnHamburger");
+	const body = document.querySelector("body");
+	const header = document.querySelector(".header");
+	const footer = document.querySelector(".footer");
+	const fadeElements = document.querySelectorAll(".has-fade");
+
+	btnHamburger.onclick = () => {
 	if (header.classList.contains("open")) {
-	  header.classList.remove("open");
-	  body.classList.remove("no-scroll");
-	  fadeElements.forEach((element) => {
+		header.classList.remove("open");
+		body.classList.remove("no-scroll");
+		fadeElements.forEach((element) => {
 		element.classList.remove("fade-in");
 		element.classList.add("fade-out");
-	  });
+		});
 
-	  footer.classList.remove("fixed-bottom");
+		footer.classList.remove("fixed-bottom");
 	} else {
-	  header.classList.add("open");
-	  body.classList.add("no-scroll");
-	  fadeElements.forEach((element) => {
+		header.classList.add("open");
+		body.classList.add("no-scroll");
+		fadeElements.forEach((element) => {
 		element.classList.remove("fade-out");
 		element.classList.add("fade-in");
-	  });
+		});
 
-	  footer.classList.add("fixed-bottom");
+		footer.classList.add("fixed-bottom");
 	}
 
 	if (document.title == "Experience | Priyansh Bordia")
@@ -47,5 +48,9 @@ document.addEventListener("DOMContentLoaded", () => {
 			experience.style.opacity = "0";
 		}
 	}
-  };
+	};
+
+	const footer__grid__text = document.querySelector(".footer__grid__text .has-hover")
+	console.log(footer__grid__text);
+	footer__grid__text.innerHTML = "By Priyansh Bordia &copy; 2020-" + new Date().getFullYear();
 });
